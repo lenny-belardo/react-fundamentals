71623635e8bac6b06c89955803485200a0cd0af9
x-factor <raoul@appfactor.io>
2024-09-06 14:07:33 +0300

    forms: controlled the valu of the input using React's state

--- src/exercise/06.js
+++ src/exercise/06.js
@@ -5,28 +5,25 @@ import * as React from 'react'
 
 function UsernameForm({onSubmitUsername}) {
   const inputRef = React.useRef()
-  const [error, setError] = React.useState(null)
+  const [username, setUsername] = React.useState('')
 
   function handleSubmit(event) {
     event.preventDefault()
 
-    const username = event.target.elements.usernameInput.value
     onSubmitUsername(username)
   }
 
   function handleOnChange(event) {
-    const isValid = event.target.value === event.target.value.toLowerCase()
-    setError(isValid ? null : 'Username must be lower case')
+    setUsername(event.target.value.toLowerCase())
   }
 
   return (
     <form onSubmit={handleSubmit}>
       <div>
         <label htmlFor="usernameInput">Username:</label>
-        <input ref={inputRef} id="usernameInput" type="text" onChange={handleOnChange} />
+        <input ref={inputRef} id="usernameInput" type="text" value={username} onChange={handleOnChange} />
       </div>
-      {error && <div role='alert' style={{color: 'red'}}>{error}</div>}
-      <button type="submit" disabled={Boolean(error)}>Submit</button>
+      <button type="submit">Submit</button>
     </form>
   )
 }
